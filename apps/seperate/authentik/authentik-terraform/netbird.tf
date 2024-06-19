6a834a9fe42f04dd37bf1001f8c147d10a3f6b0d
Cal <tilapia.repair.0t@icloud.com>
2024-06-19 22:23:59 +0100

    remove path

--- apps/seperate/authentik/authentik-terraform/netbird.tf
+++ apps/seperate/authentik/authentik-terraform/netbird.tf
@@ -38,7 +38,6 @@ resource "authentik_user" "netbird" {
   username = "Netbird"
   name     = "Netbird"
   type     = "service_account"
-  path     = "goauthentik.io/service-accounts"
   password = data.kubernetes_secret.terraform-config-secrets.data.netbird-token
   groups = [data.authentik_group.akadmins.id]
   attributes = "{'goauthentik.io/user/token-expires': false}"
