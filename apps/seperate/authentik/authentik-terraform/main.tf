1f7ebb70faeab1ab648656063893a1da94252dcd
Cal <github@dev.cinderlab.com>
2024-09-08 15:27:57 +0100

    update to new scope mapping

--- apps/seperate/authentik/authentik-terraform/main.tf
+++ apps/seperate/authentik/authentik-terraform/main.tf
@@ -47,24 +47,24 @@ data "authentik_certificate_key_pair" "generated" {
 }
 
 # Defualts
-data "authentik_scope_mapping" "scope-email" {
+data "authentik_property_mapping_provider_scope" "scope-email" {
   name = "authentik default OAuth Mapping: OpenID 'email'"
 }
 
-data "authentik_scope_mapping" "scope-profile" {
+data "authentik_property_mapping_provider_scope" "scope-profile" {
   name = "authentik default OAuth Mapping: OpenID 'profile'"
 }
 
-data "authentik_scope_mapping" "scope-openid" {
+data "authentik_property_mapping_provider_scope" "scope-openid" {
   name = "authentik default OAuth Mapping: OpenID 'openid'"
 }
 
-data "authentik_scope_mapping" "scope-offline-access" {
+data "authentik_property_mapping_provider_scope" "scope-offline-access" {
   name = "authentik default OAuth Mapping: OpenID 'offline_access'"
 }
 
 # Create a scope mapping
-resource "authentik_scope_mapping" "gitea" {
+resource "authentik_property_mapping_provider_scope" "gitea" {
   name       = "authentik gitea OAuth Mapping: OpenID 'gitea'"
   scope_name = "gitea"
   expression = <<EOF
